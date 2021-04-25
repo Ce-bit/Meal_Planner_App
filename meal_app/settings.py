fe6b596d041329afc4f9b4f1be55ed9c76515165
Ce-bit <jamal.jov.ali@gmail.com>
2021-04-25 05:44:48 -0400

    idk

--- meal_app/settings.py
+++ meal_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'meal_api',
     'rest_framework',
+    'corsheaders',                              # add this
     'meal_frontend',
     'knox',
     'accounts',
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',    # add this
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,5 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CORS_ORIGIN_ALLOW_ALL = True
 django_heroku.settings(locals())
